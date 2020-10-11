13f9d49a72597184f3ecbaa333942176bcb6eaec
Suyash906 <suyash90682711@gmail.com>
2020-10-11 10:55:52 -0700

    persistent changes across reload

--- components/PlaintextEditor/index.js
+++ components/PlaintextEditor/index.js
@@ -1,15 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import path from 'path';
 
 import css from './style.css';
 
 function PlaintextEditor({ file, write }) {
   console.log(file, write);
+
+  const [value, setValue] = useState('');
+
+  useEffect(() => {
+    (async () => {
+      setValue(await file.text());
+    })();
+  }, []);
+
+  const handletextChange = (e) => {
+    console.log(`file changed`)
+    setValue(e.target.value)
+  }
+
+  const updateText = (e) => {
+    e.preventDefault();
+
+    let new_file = new File([value],
+      file.name,
+      {
+        type: 'text/plain',
+        lastModified: new Date(Date.now())
+      }
+    );
+
+    write(new_file)
+
+  }
+
   return (
-    <div className={css.editor}>
-      <h3>TODO</h3>
-      <i>text/plain</i>
+    <>
+    <form onSubmit={updateText}>
+      <textarea rows="20" cols="60" name="value" value={value} onChange={handletextChange}></textarea>
+      <button type="submit"> Save </button>
+    </form>
+    <div className={css.preview}>
+      <div className={css.title}>{path.basename(file.name)}</div>
+      <div className={css.content}>{value}</div>
     </div>
+    </>
   );
 }
 
